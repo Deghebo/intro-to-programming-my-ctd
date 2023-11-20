153eaa93bb074e4c065f14f9be511eeee1f6305f
Deghebo <102492196+Deghebo@users.noreply.github.com>
2023-11-20 07:17:52 -0500

    add footer and script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+//const body = document.body;
+
+document.body.addEventListener("click", () => {
+  console.log("You clicked the body of the page!")
+  //body.innerHTML = "<h1>Hello, World!</h1>";
+});
