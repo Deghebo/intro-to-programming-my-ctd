b958363163f759823a2661955f1ccbcb14ebff75
Deghebo <102492196+Deghebo@users.noreply.github.com>
2023-11-22 22:26:42 -0500

    checked out lesson 4.2
    checked out lesson 4.2 from main

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const today = newDate();
+let thisYear = today.getFullYear();
+const footer = document.querySelector("footer");
+var copyright = document.createElement('<p>Chalew Deghebo</p>').thisYear;
+document.getElementById('footer').appendChild(copyright);
