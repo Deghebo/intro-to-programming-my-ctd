122062705498573972f6cc0c5b988deb01781f4f
Chalew Deghebo <chalewd@gmail.com>
2023-11-27 15:14:22 -0500

    add footer element and skills section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const today = newDate();
+const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
-const copyright = document.createElement('<p>Chalew Deghebo</p>').thisYear;
-document.getElementById('footer').appendChild(copyright);
+const copyright = document.createElement('p'); 
+document.getElementById('p').appendChild(copyright).innerHTML = `Chalew Degehebo ' + ' thisYear`;
+
+const skills = ['HTML', 'CSS', 'JavaScript', 'bubble.io'];
+
+const skillsSection = document.getElementById('skills');
+ 
+const skillsList = for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    const skill = createElement.listItem('li');
+};
+
+document.getElementById().appendChild(skill)
