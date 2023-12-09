dc7478c2ba44967ab5dfe533e21240560305261b
Deghebo <102492196+Deghebo@users.noreply.github.com>
2023-12-09 13:03:48 -0500

    fixed merge conflict and overall  code errors

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
-const copyright = document.createElement('p'); 
+const copyright = document.createElement('p');
 //document.getElementById('p').appendChild(copyright).innerHTML = `Chalew Degehebo ' + ' thisYear`;
 
 copyright.innerHTML = `&copy; ${thisYear} by Chalew Deghebo. All rights reserved. `
@@ -11,12 +11,15 @@ footer.appendChild(copyright)
 const skills = ['HTML', 'CSS', 'JavaScript', 'bubble.io'];
 
 const skillsSection = document.getElementById('skills');
- 
+
 const skillsList = skillsSection.querySelector('ul')
 
 for (let index = 0; index < skills.length; index++) {
-    
+
     const skill = document.createElement('li');
     skill.innerText = skills[index]
     skillsList.appendChild(skill)
 };
+
+const messageForm = document.getElementsByName('leave_message')
+addEventListener.button('submit')
