e29f1484034f337ac936895bf1a0110c01b975fb
Deghebo <102492196+Deghebo@users.noreply.github.com>
2023-12-11 23:04:56 -0500

    add DOM selection code to message section

--- js/index.js
+++ js/index.js
@@ -20,6 +20,47 @@ for (let index = 0; index < skills.length; index++) {
     skill.innerText = skills[index]
     skillsList.appendChild(skill)
 };
+// Handle message form Submit
+const messageForm = document.getElementsByName('leave_message');
+console.log(messageForm);
+console.log(messageForm[0]);
+// const input = form.querySelector('input');
 
-const messageForm = document.getElementsByName('leave_message')
-addEventListener.button('submit')
+messageForm[0].addEventListener('submit', (event) => {
+    event.preventDefault(); //prevent default browser refresh event
+    // obtain values of the form 
+    const firstName = event.target.usersFirstName.value;
+    const lastName = event.target.usersLastName.value;
+    const email = event.target.usersEmail.value;
+    const userMessage = event.target.usersMessage.value;
+    console.log(firstName, lastName, email, userMessage); /* register values in console*/
+    /* console.log(input.value); */
+    messageForm.reset();  /* clear the form fields */
+    /* create list display message */
+    const messageSection = document.geteElementById('messages');
+    const messageList = messageSection.querySelector('ul')
+    const newMessage = document.createElement('li');
+    // newMessage.innerText = `${<a></a>}`
+
+    onsubmit = (event) => { };
+
+    // newMessage.innerText = `${name} (${email}): ${userMessage}`;
+    messageList.appendChild(newMessage)
+
+    const removeButton = document.createElement('Button');
+    removeButton.innerText = remove;
+    removeButton.addEventListener('remove', (event) => {
+        const entry = document.querySelector('li');
+    })
+})
+
+// Get all elements with the name "exampleName"
+// const elements = document.getElementsByName("exampleName");
+
+// Loop through the collection and add an event listener to each element
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].addEventListener("click", function () {
+//         // Your event handler code here
+//         console.log(`Element ${i} clicked`);
+//     });
+// }
