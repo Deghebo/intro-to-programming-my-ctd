0513bf88ff7f4a19b2f5bf1fd7e4210b51c2a3a2
Deghebo <102492196+Deghebo@users.noreply.github.com>
2023-12-29 03:23:57 -0500

    add styles to message section elements

--- script.js
+++
@@ -1,6 +0,0 @@
-//const body = document.body;
-
-//document.body.addEventListener("click", () => {
-// console.log("You clicked the body of the page!")
-//body.innerHTML = "<h1>Hello, World!</h1>";
-//});
